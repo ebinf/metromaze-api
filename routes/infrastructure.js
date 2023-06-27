f7eafc8a13329926f3e3da8f1c9ca9332503118d
Erik Borowski <erik@ebinf.eu>
2023-06-27 11:45:01 +0200

    Split zHV to separate providers

--- routes/infrastructure.js
+++ routes/infrastructure.js
@@ -1,6 +1,11 @@
 import express from "express";
+import providers from "../assets/providers/_providers.json" assert { type: "json" };
 
 export const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json(providers);
+});
 
 router.get("/:provider", (req, res) => {
   const provider = req.params.provider.toLowerCase();
@@ -8,17 +13,22 @@ router.get("/:provider", (req, res) => {
     res.status(400).json({ error: "No provider specified" });
   }
 
-  switch (provider) {
-    case "rmv":
-      res.sendFile("rmv.json", { root: "assets/providers" });
-      break;
-
-    case "zhv":
-      res.sendFile("zhv.json", { root: "assets/providers" });
-      break;
-
-    default:
-      res.status(400).json({ error: "Unknown provider" });
-      break;
+  if (!provider in providers) {
+    res.status(404).json({ error: "Provider not found" });
   }
+
+  res.sendFile(`${provider}.json`, { root: "assets/providers" });
+});
+
+router.get("/:provider/meta", (req, res) => {
+  const provider = req.params.provider.toLowerCase();
+  if (provider === undefined) {
+    res.status(400).json({ error: "No provider specified" });
+  }
+
+  if (!provider in providers) {
+    res.status(404).json({ error: "Provider not found" });
+  }
+
+  res.json(providers[provider]);
 });
--- scripts/zhv_to_json.js
+++ scripts/zhv_to_json.js
@@ -1,16 +1,69 @@
 import fs from "fs";
 import { XMLParser } from "fast-xml-parser";
+import csv from "csvtojson";
+
+const parser = new XMLParser();
+
+function nameReplace(name) {
+  if (name === "rms") return "RMV";
+  return name;
+}
+
+function shortnameReplace(name) {
+  if (name === "rms") return "rmv";
+  if (name === "Hamburger Hochbahn") return "hha";
+  return name.toLocaleLowerCase();
+}
 
 async function main() {
   const file = process.argv[2];
   if (!file) {
     throw new Error("No file specified");
   }
-  const parser = new XMLParser();
+
+  const replacements_file = process.argv[3];
+  if (!replacements_file) {
+    console.log("No replacements file specified");
+  }
+  const replacements_data = parser.parse(
+    fs.readFileSync(replacements_file, "utf8")
+  );
+  var replacements = {};
+  if (replacements_data?.Items?.StopPlace) {
+    var stopplaces = replacements_data?.Items?.StopPlace;
+    if (!Array.isArray(replacements_data?.Items?.StopPlace)) {
+      stopplaces = [replacements_data.Items.StopPlace];
+    }
+    for (const row of stopplaces) {
+      replacements[row?.DHID] = row;
+    }
+  }
+
+  const beacons_file = process.argv[4];
+  if (!beacons_file) {
+    console.log("No beacons file specified");
+  }
+  const beacons_data = await csv({
+    delimiter: ",",
+  }).fromFile(beacons_file);
+  var beacons = {};
+  for (const row of beacons_data) {
+    if (beacons[row["HAFAS"]] === undefined) {
+      beacons[row["HAFAS"]] = [];
+    }
+    beacons[row["HAFAS"]].push(row["Beacon"]);
+  }
+
   const jsonData = parser.parse(fs.readFileSync(file, "utf8"));
-  var relevantInfo = [];
-  for (const row of jsonData?.GetResponseType?.Items?.StopPlace) {
-    if (!row?.DHID.startsWith("de:06411:")) continue;
+  var relevantInfo = {};
+  var metaInfo = {};
+  for (var row of jsonData?.GetResponseType?.Items?.StopPlace) {
+    if (row?.DHID in replacements) {
+      row = replacements[row?.DHID];
+      console.log(`Found replacement for ${row?.DHID} (${row?.Name?.Name})`);
+    }
+    if (row?.Condition?.ConditionKind === "Unserved") continue;
+    if (row?.State?.StateKind === "OutOfOrder") continue;
     var locations = [];
     if (row?.AreaQuay?.Items?.Area) {
       var areas = row?.AreaQuay?.Items?.Area;
@@ -18,12 +71,14 @@ async function main() {
         areas = [row?.AreaQuay?.Items?.Area];
       }
       for (const area of areas) {
+        if (area?.Condition?.ConditionKind === "Unserved") continue;
+        if (area?.State?.StateKind === "OutOfOrder") continue;
         if (!area?.Quays?.Quay) {
-          locations.push({
-            name: "area | " + area?.Name?.Name,
-            latitude: parseFloat(area?.Location?.Latitude),
-            longitude: parseFloat(area?.Location?.Longitude),
-          });
+          locations.push(
+            `${parseFloat(area?.Location?.Latitude)},${parseFloat(
+              area?.Location?.Longitude
+            )}`
+          );
           continue;
         }
         var quays = area?.Quays?.Quay;
@@ -31,34 +86,48 @@ async function main() {
           quays = [area?.Quays?.Quay];
         }
         for (const quay of quays) {
-          locations.push({
-            name: "quay | " + quay?.Name?.Name,
-            latitude: parseFloat(quay?.Location?.Latitude),
-            longitude: parseFloat(quay?.Location?.Longitude),
-          });
+          if (quay?.Condition?.ConditionKind === "Unserved") continue;
+          if (quay?.State?.StateKind === "OutOfOrder") continue;
+          locations.push(
+            `${parseFloat(quay?.Location?.Latitude)},${parseFloat(
+              quay?.Location?.Longitude
+            )}`
+          );
         }
       }
     } else {
-      locations = [
-        {
-          name: "stop | " + row?.Name?.Name,
-          latitude: parseFloat(row?.Location?.Latitude),
-          longitude: parseFloat(row?.Location?.Longitude),
-        },
-      ];
+      locations.push(
+        `${parseFloat(row?.Location?.Latitude)},${parseFloat(
+          row?.Location?.Longitude
+        )}`
+      );
     }
-    var beacons = [];
-    relevantInfo.push({
-      id: row?.DHID,
-      name: row?.Name?.Name,
-      locations: locations,
-      beacons: beacons,
+    if (relevantInfo[row?.Authority?.Label] === undefined) {
+      relevantInfo[row?.Authority?.Label] = [];
+    }
+    relevantInfo[row?.Authority?.Label].push({
+      i: row?.DHID,
+      n: row?.Name?.Name,
+      l: locations,
+      ...(row?.DHID in beacons ? { b: beacons[row?.DHID] } : {}),
     });
   }
+  for (const key in relevantInfo) {
+    fs.writeFileSync(
+      `assets/providers/${shortnameReplace(key)}.json`,
+      JSON.stringify(relevantInfo[key], null, 2),
+      "utf8"
+    );
+    metaInfo[shortnameReplace(key)] = {
+      name: nameReplace(key),
+      stations: relevantInfo[key].length,
+      updated: new Date(),
+      size: fs.statSync(`assets/providers/${shortnameReplace(key)}.json`).size,
+    };
+  }
   fs.writeFileSync(
-    "assets/providers/zhv.json",
-    JSON.stringify(relevantInfo, null, 2),
-    // JSON.stringify(jsonData, null, 2),
+    `assets/providers/_providers.json`,
+    JSON.stringify(metaInfo, null, 2),
     "utf8"
   );
 }
